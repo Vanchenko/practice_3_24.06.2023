367ac6d0df27422f9f095a481afa436299b6991a
Hennadii Yarosh <nedinix.tech@gmail.com>
2023-06-24 12:24:57 +0300

    task-5

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,8 +1,12 @@
-import { Section, Container, CountryInfo, Loader } from 'components';
+import { Section, Container, CountryInfo, Loader, GoBackBtn } from 'components';
 import { useFetchCountry } from 'hooks/useFetchCountry';
+import { useLocation } from 'react-router-dom';
+import { routes } from 'routes';
 
 export const Country = () => {
   const { country, isLoading, error } = useFetchCountry();
+  const location = useLocation();
+  console.log(location);
   const {
     flag,
     capital,
@@ -11,19 +15,23 @@ export const Country = () => {
     languages = [],
     population,
   } = country;
+  const goBackLocation = location?.state?.from ?? routes.HOME;
   return (
     <Section>
       <Container>
         {isLoading && <Loader />}
         {error && <p>There is no country yet...</p>}
-        {country.hasOwnProperty('flag') && <CountryInfo
-          flag={flag}
-          capital={capital}
-          id={id}
-          country={countryName}
-          languages={languages}
-          population={population}
-        />}
+        <GoBackBtn path={goBackLocation}>Go back</GoBackBtn>
+        {country.hasOwnProperty('flag') && (
+          <CountryInfo
+            flag={flag}
+            capital={capital}
+            id={id}
+            country={countryName}
+            languages={languages}
+            population={population}
+          />
+        )}
       </Container>
     </Section>
   );
